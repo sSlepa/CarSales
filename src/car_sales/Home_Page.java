3db26c392a16b8aa9f2efee15a89b3a6561011ea
Slepa <adriangaspar71@gmail.com>
2024-10-24 18:28:23 +0300

    Commit checker register default strings

--- src/car_sales/Home_Page.java
+++ src/car_sales/Home_Page.java
@@ -36,6 +36,7 @@ public class Home_Page extends javax.swing.JFrame {
         Login_HomeButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(51, 204, 255));
